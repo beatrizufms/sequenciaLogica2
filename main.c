f8cc91bb278986f7f8c5b3a017bc7920c9cde243
Beatriz <89365231+beatrizufms@users.noreply.github.com>
2021-09-04 18:57:00 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*Escreva um programa que leia dois valores X e Y.
+A seguir, mostre uma sequência de 1 até Y,
+passando para a próxima linha a cada X números.*/
+int main(void){
+    int n, x1;
+    int cont = 1, aux = 1;
+    scanf("%i %i", &n, &x1);
+
+    for(int i = 1; i <= x1; i++){
+        if(cont < n){
+            printf("%i ", aux);
+            cont++;
+            aux++;
+        }else{
+            printf("%i\n", aux);
+            aux++;
+            cont = 1;
+        }
+    }
+    return 0;
+}
